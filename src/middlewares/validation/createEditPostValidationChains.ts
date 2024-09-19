fc8778abd50e4beaf82f4282fc45a77bc4622e10
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2024-09-19 23:11:55 +0400

    fix: added trim to validation body fields

--- src/middlewares/validation/createEditPostValidationChains.ts
+++ src/middlewares/validation/createEditPostValidationChains.ts
@@ -3,23 +3,27 @@ import { validationMiddleware } from "./validationMiddleware";
 import { blogsRepository } from "../../repositories/blogsRepository";
 
 const createTitleChain = () => body('title')
-    .notEmpty().withMessage('Should not be empty')
     .isString().withMessage('Should be a string')
+    .trim()
+    .notEmpty().withMessage('Should not be empty')
     .isLength({ max: 30 }).withMessage('Max - 30 symbols');
 
 const createDescriptionChain = () => body('shortDescription')
-    .notEmpty().withMessage('Should not be empty')
     .isString().withMessage('Should be a string')
+    .trim()
+    .notEmpty().withMessage('Should not be empty')
     .isLength({ max: 100 }).withMessage('Max - 100 symbols');
 
 const createContentChain = () => body('content')
-    .notEmpty().withMessage('Should not be empty')
     .isString().withMessage('Should be a string')
+    .trim()
+    .notEmpty().withMessage('Should not be empty')
     .isLength({ max: 1000 }).withMessage('Max - 1000 symbols');
 
 const createBlogIdChain = () => body('blogId')
-    .notEmpty().withMessage('Should not be empty')
     .isString().withMessage('Should be a string')
+    .trim()
+    .notEmpty().withMessage('Should not be empty')
     .custom(value => {
         const foundBlog = blogsRepository.getBlogById(value);
 

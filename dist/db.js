de7cf78fa8f0b11ad3c45867f92117ef8146a8b7
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2024-09-25 11:40:05 +0400

    fix git ignore

--- dist/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.blogsDB = exports.postsDB = void 0;
-const postsDB = [];
-exports.postsDB = postsDB;
-const blogsDB = [];
-exports.blogsDB = blogsDB;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config({ path: '.env.local' });
-const app_1 = require("./app");
-const config_1 = require("./utils/config");
-app_1.app.listen(config_1.CONFIG.PORT, () => {
-    console.log("App listening on port ", config_1.CONFIG.PORT);
-});

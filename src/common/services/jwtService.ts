71eb265406a3fa360c66b67796edbd7bac1e6569
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2024-10-31 22:46:43 +0400

    feature: return back expDate for tokens

--- src/common/services/jwtService.ts
+++ src/common/services/jwtService.ts
@@ -8,7 +8,7 @@ const secret = CONFIG.JWT_SECRET
 export const jwtAuthService = {
     createAccessToken(user: any): string {
         const token = jwt.sign(user, String(secret), {
-            expiresIn: '20s'
+            expiresIn: '10s'
         });
 
         return token;
@@ -16,7 +16,7 @@ export const jwtAuthService = {
 
     createRefreshToken(user: any): string {
         const token = jwt.sign(user, String(secret), {
-            expiresIn: '10m'
+            expiresIn: '20s'
         });
 
         return token;
